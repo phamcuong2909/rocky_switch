254bf8c40c46f8e341eb28279ce82cf11e61cb4e
hvminh <hvminh@tma.com.vn>
2016-08-08 08:53:20 +0700

    Update the code to handle button debounce better

--- Esp8266_RockySwitch/Esp8266_RockySwitch.ino
+++ Esp8266_RockySwitch/Esp8266_RockySwitch.ino
@@ -11,53 +11,58 @@
 const char* ssid = "Sandiego";
 const char* password = "0988807067";
 
-const char* clientId = "RockySwitch1";
+const char* clientId = "RockySwitch2";
 const char* mqttServer = "192.168.1.110";
 const char* mqttUsername = "<MQTT_BROKER_USERNAME>";
 const char* mqttPassword = "<MQTT_BROKER_PASSWORD>";
 const char* relayStatusTopics[] = {
-  "/Bedroom1/Light1/Status", 
-  "/Bedroom1/Light2/Status",
-  "/Bedroom1/Light3/Status"
+  "/Livingroom/Light1/Status", 
+  "/Livingroom/Light2/Status",
+  "/Livingroom/Light3/Status",
+  "/Livingroom/Light4/Status"
 }; // should change to something unique
 
 const char* relayCmdTopics[] = {
-  "/Bedroom1/Light1/Command", 
-  "/Bedroom1/Light2/Command",
-  "/Bedroom1/Light3/Command"
+  "/Livingroom/Light1/Command", 
+  "/Livingroom/Light2/Command",
+  "/Livingroom/Light3/Command",
+  "/Livingroom/Light4/Command"
 }; // should change to something unique
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-unsigned long lastReconnectTime;
+unsigned long lastReconnectTime = 0;
 const long reconnectInterval = 30000;
 
-const byte buttonPins[3] = {D0, D1, D2};
-const byte relayPins[3]  = {D7, D6, D5};
-byte relayStatus[3] = {0, 0, 0};
+const byte buttonPins[4] = {D0, D5, D6, D7};
+const byte relayPins[4]  = {D1, D2, D3, D4};
+byte relayStatus[4] = {0, 0, 0, 0};
 
 // used for button debouncing
-const byte debounceDelay = 50; // Debounce time;
+const byte debounceDelay = 100; // Debounce time;
 unsigned long lastDebounceTime = 0; //The last time button was toggled
 boolean buttonPressed = false;
-byte currentButtonStatus[3];
-byte lastButtonStatus[3];
+byte currentButtonStatus[4];
+byte lastButtonStatus[4];
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
+
+  Serial.println("Rocky Switch is starting");
 
   // Configure input pins and output pins
   for(int i=0; i<sizeof(relayPins); i++)
   {
     pinMode(relayPins[i], OUTPUT); // Configure pin2,3,4 as an output 
     pinMode(buttonPins[i], INPUT); // Configure pin5,6,7 as an pull-up input
-    digitalWrite(relayPins[i], LOW); //init OFF all relay
+    digitalWrite(relayPins[i], HIGH); //init OFF all relay
     lastButtonStatus[i] = 0;
   }
 
   setup_wifi();
 
+  /*
   // Port defaults to 8266
   // ArduinoOTA.setPort(8266);
 
@@ -86,25 +91,30 @@ void setup() {
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
   ArduinoOTA.begin();
+  */
 
   client.setServer(mqttServer, 1883);
   client.setCallback(onMessageReceived);
+  reconnect();
 
   // Waiting for connection ready before sending update
-  delay(500); 
-  sendStatusUpdate();
+  //delay(500); 
+  //sendStatusUpdate();
 }
 
 void loop() {
-  ArduinoOTA.handle();
-
+  //ArduinoOTA.handle();
+  /*
   if (!client.connected()) {
     unsigned long currentMillis = millis();
+    Serial.print("Not connected: "); Serial.println(currentMillis);
     if (currentMillis - lastReconnectTime >= reconnectInterval) {
       lastReconnectTime = currentMillis;
-      reconnect();
+      Serial.println("Need to reconnect");
+      //reconnect();
     }
   }
+  */
 
   if (client.connected()) {
     client.loop();
@@ -116,6 +126,23 @@ void loop() {
   {
     byte reading = digitalRead(buttonPins[i]); // Read the state of the switch
 
+    if( reading == LOW)                 
+    {
+      relayStatus[i] = 1 - relayStatus[i];
+      digitalWrite(relayPins[i], 1 - relayStatus[i]);
+      buttonPressed = true;
+      //Print out the value of the relay status
+      Serial.print("Changed relay "); Serial.print(i); Serial.print(" status to "); Serial.println(relayStatus[i]);
+      delay(300);
+      if (buttonPins[i] == D0) {
+        pinMode(D0, OUTPUT);
+        digitalWrite(D0, 1);
+        pinMode(D0, INPUT);
+      }
+    }
+
+    /*
+
     // If the switch changed, due to noise or pressing:
     if( reading != lastButtonStatus[i])                   
     {
@@ -126,14 +153,16 @@ void loop() {
     {
       // Check button is clicked, change status of relay
       if( reading == HIGH)                 
-      { 
+      {
+        Serial.println("AAAAAAAAAAAAAAAAAAAAAAAAAAA"); 
         relayStatus[i] = 1 - relayStatus[i];
-        digitalWrite(relayPins[i], relayStatus[i]);
+        digitalWrite(relayPins[i], 1 - relayStatus[i]);
         buttonPressed = true;
         //Print out the value of the relay status
         Serial.print("Changed relay "); Serial.print(i); Serial.print(" status to "); Serial.println(relayStatus[i]);
       }
     }
+    */
     lastButtonStatus[i] = reading;
   }
 
@@ -148,8 +177,6 @@ void setup_wifi() {
   Serial.print("Connecting to "); Serial.println(ssid);
 
   WiFi.begin(ssid, password);
-  //uncomment to use static ip to shorten config time
-  //WiFi.config(IPAddress(192,168,1,220), IPAddress(192,168,1,100), IPAddress(255,255,255,0));
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(300);
@@ -175,11 +202,11 @@ void onMessageReceived(char* topic, byte* payload, unsigned int length) {
       Serial.print("Received message for relay "); Serial.println(i);
       if ((char)payload[0] == '1') {
         Serial.println("Command is to turn on");
-        digitalWrite(relayPins[i], HIGH);
+        digitalWrite(relayPins[i], LOW);
         relayStatus[i] = HIGH;
       } else if ((char)payload[0] == '0') {
         Serial.println("Command is to turn off");
-        digitalWrite(relayPins[i], LOW);
+        digitalWrite(relayPins[i], HIGH);
         relayStatus[i] = LOW;
       } else {
         Serial.print("Invalid command received: "); Serial.println(payload[0]);
@@ -189,6 +216,7 @@ void onMessageReceived(char* topic, byte* payload, unsigned int length) {
   }
   //sendStatusUpdate();
 }
+
 
 void reconnect() {
   Serial.print("Attempting MQTT connection...");
@@ -202,15 +230,17 @@ void reconnect() {
       delay(200);
     }
     Serial.println("Subscribed all topics to broker");
-    sendStatusUpdate();
+    //sendStatusUpdate();
   } else {
     Serial.print("failed, rc="); Serial.println(client.state());
   }
 }
 
+
 /*  when a relay is manually switched by button, this sends all status update
     to server
 */
+
 void sendStatusUpdate()
 {  
   for(int i=0; i<sizeof(relayStatus); i++) {
@@ -218,7 +248,7 @@ void sendStatusUpdate()
     char status [4];
     sprintf (status, "%d", relayStatus[i]);
     client.publish(relayStatusTopics[i], status);
-    delay(100);
+    delay(50);
   }
 }
 
